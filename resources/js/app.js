1ffa6a8311521dd3c4887a8494456b8a3e15ae31
Adam Stafford <adam.stafford@bettercarpeople.com>
2018-09-28 12:24:49 -0400

    todays progress. grids. update from removing bootstrap. overlay. forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
-import 'bootstrap';
 
-$(document).ready(function($) {
+$(document).ready(function ($) {
 
 	//Modal
-	$('#myModal').on('shown.bs.modal', function() {
+	$('#myModal').on('shown.bs.modal', function () {
 		$('#myInput').focus()
 	});
 
 	//Dropdown on Click
-	function dropDown() {
-		var x = document.getElementById("sub-menu");
-		if (x.className.indexOf("show") == -1) {
-			x.className += " show";
-		} else {
-			x.className = x.className.replace(" show", "");
+	$('.submenu-trigger').on('click', function() {
+		var subMenu = $(this).parent().find('.sub-menu');
+		if (subMenu.hasClass('show')) {
+			subMenu.removeClass('show')
 		}
-	}
+		else {
+			subMenu.addClass('show');
+		}
+	});
 
 	//Simple Sidebar Toggle
 	$("#menu-toggle").click(function (e) {
@@ -33,7 +33,7 @@ $(document).ready(function($) {
 	// Remove links that don't actually link to anything
 		.not('[href="#"]')
 		.not('[href="#0"]')
-		.click(function(event) {
+		.click(function (event) {
 			// On-page links
 			if (
 				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
@@ -49,7 +49,7 @@ $(document).ready(function($) {
 					event.preventDefault();
 					$('html, body').animate({
 						scrollTop: target.offset().top
-					}, 1000, function() {
+					}, 1000, function () {
 						// Callback after animation
 						// Must change focus!
 						var $target = $(target);
@@ -57,9 +57,10 @@ $(document).ready(function($) {
 						if ($target.is(":focus")) { // Checking if the target was focused
 							return false;
 						} else {
-							$target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+							$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
 							$target.focus(); // Set focus again
-						};
+						}
+						;
 					});
 				}
 			}
