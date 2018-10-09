a7edcb9b133560c639b87c9a1651559f3feabc7f
adamestafford <adamestafford@me.com>
2018-10-08 21:32:18 -0400

    migrating ricks files/configuring/styling

--- public/js/app.js
+++ public/js/app.js
@@ -84,9 +84,31 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 __WEBPACK_IMPORTED_MODULE_0_jquery___default()(document).ready(function ($) {
 
 	//Modal
-	$('#myModal').on('shown.bs.modal', function () {
-		$('#myInput').focus();
-	});
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on the button, open the modal
+	btn.onclick = function () {
+		modal.style.display = "block";
+	};
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function () {
+		modal.style.display = "none";
+	};
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	};
 
 	//Feather Icons initialize
 	feather.replace();
